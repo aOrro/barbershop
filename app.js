70bb7b5aceb29c4c123ed214841d4a5c2becc600
Anton Orro <65253837+reekArd@users.noreply.github.com>
2020-11-01 19:45:01 +0000

    Create app.js
    Funcionalidades para la sección de reviews (efecto carousel) y el mapa de localización de Google

---
+++ app.js
@@ -0,0 +1,43 @@
+// Reviews Carousel effect
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("social-reviews");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "flex";
+  dots[slideIndex-1].className += " active";
+}
+
+// Reviews Carousel effect END
+
+// Google Location Map
+
+function myMap() {
+let mapProp= {
+  center:new google.maps.LatLng(51.435400,-0.159940),
+  zoom:5,
+};
+let map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+
+// Google Location Map END
