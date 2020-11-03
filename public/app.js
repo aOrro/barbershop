6cac1ebc96aea6ca28a703d157b02e78e20d6c42
Anton Orro <aotab91@gmail.com>
2020-11-03 17:12:27 +0000

    commit

--- public/app.js
+++ public/app.js
@@ -16,14 +16,18 @@ function showSlides(n) {
   let slides = document.getElementsByClassName("social-reviews");
   let dots = document.getElementsByClassName("dot");
 
-  if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex-1].style.display = "flex";
   dots[slideIndex-1].className += " active";
 }
@@ -39,3 +43,5 @@ let mapProp= {
 };
 let map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
+
+// Google Location Map END
